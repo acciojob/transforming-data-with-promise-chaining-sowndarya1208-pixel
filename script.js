5fc60006cd68ec07364fb3a52cbee640b7f82522
sowndarya1208-pixel <sowndarya1208@gmail.com>
2025-12-31 18:45:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,31 +2,40 @@ const input = document.getElementById("ip");
 const button = document.getElementById("btn");
 const output = document.getElementById("output");
 
-function delay(value, time) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(value);
-    }, time);
-  });
+function delay(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
 
 button.addEventListener("click", () => {
   output.innerText = "";
 
-  delay(Number(input.value), 2000)
-    .then((num) => {
+  delay(2000)
+    .then(() => {
+      const num = Number(input.value);
       output.innerText = `Result: ${num}`;
-      return delay(num * 2, 2000);
+      return num * 2;
     })
     .then((num) => {
-      output.innerText = `Result: ${num}`;
-      return delay(num - 3, 1000);
+      return delay(1000).then(() => {
+        output.innerText = `Result: ${num}`;
+        return num - 3;
+      });
     })
     .then((num) => {
-      output.innerText = `Result: ${num}`;
-      return delay(num / 2, 1000);
+      return delay(1000).then(() => {
+        output.innerText = `Result: ${num}`;
+        return num / 2;
+      });
     })
     .then((num) => {
-      output.innerText = `Final Result: ${num + 10}`;
+      return delay(1000).then(() => {
+        output.innerText = `Result: ${num}`;
+        return num + 10;
+      });
+    })
+    .then((final) => {
+      return delay(1000).then(() => {
+        output.innerText = `Final Result: ${final}`;
+      });
     });
 });
