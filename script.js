e297f746aa410c5cbe3cb7253300f0c67d85f69e
sowndarya1208-pixel <sowndarya1208@gmail.com>
2025-12-31 18:35:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+const input = document.getElementById("ip");
+const button = document.getElementById("btn");
+const output = document.getElementById("output");
+
+function delay(value, time) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(value);
+    }, time);
+  });
+}
+
+button.addEventListener("click", () => {
+  output.innerText = "";
+
+  delay(Number(input.value), 2000)
+    .then((num) => {
+      output.innerText = `Result: ${num}`;
+      return delay(num * 2, 2000);
+    })
+    .then((num) => {
+      output.innerText = `Result: ${num}`;
+      return delay(num - 3, 1000);
+    })
+    .then((num) => {
+      output.innerText = `Result: ${num}`;
+      return delay(num / 2, 1000);
+    })
+    .then((num) => {
+      output.innerText = `Final Result: ${num + 10}`;
+    });
+});
